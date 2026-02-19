aa42a5b24833bf4016dc59e35c10cbac58b7967a
Soe Moe Naing <kosoemoe2004@gmail.com>
2026-02-19 17:33:36 +0700

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,6 @@
+print("Hello, my name is Soe Moe Naing")
+print("Computer Science student at Rangsit University")
+print("Welcome to my programming portfolio")
+
+name = input("What is your name? ")
+print("Nice to meet you, " + name + "!")
